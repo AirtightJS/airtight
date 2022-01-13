3d8d491b3695f69aaf1bfc7a5c9d9b3a9c7f2ff6
Boris Okunskiy <boris@okunskiy.name>
2022-01-13 12:02:37 +0300

    chore: refMap -> map

--- src/main/store.ts
+++ src/main/store.ts
@@ -3,12 +3,12 @@ import { SchemaDecoder } from './decoder';
 import { Schema } from './schema';
 
 export class SchemaStore {
-    protected refMap = new Map<string, Schema<unknown>>();
+    protected map = new Map<string, Schema<unknown>>();
 
     constructor(readonly parent: SchemaStore | null = null) {}
 
     register<T>(schema: SchemaWithId<T>): SchemaDecoder<T> {
-        this.refMap.set(schema.id, schema);
+        this.map.set(schema.id, schema);
         return new SchemaDecoder(schema, this);
     }
 
@@ -16,14 +16,14 @@ export class SchemaStore {
         for (const schema of schemas) {
             const id = (schema as any).id;
             if (id) {
-                this.refMap.set(id, schema);
+                this.map.set(id, schema);
             }
         }
         return this;
     }
 
     get<T>(schemaId: string): Schema<T> | null {
-        const schema = this.refMap.get(schemaId);
+        const schema = this.map.get(schemaId);
         if (schema) {
             return schema as Schema<T>;
         }
